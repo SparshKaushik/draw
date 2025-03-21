f295b0a8e682cad2f78643e83012b97b4982cb7c
Sparsh Kaushik <contact@Sparshkaushik.com>
2025-03-21 20:41:45 +0530

    refactor: improve layout component with utility functions
    * Replaced conditional rendering logic in Layout component with utility function for cleaner code.
    * Updated class names for better styling consistency based on the current route.

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,14 +1,17 @@
+import { cn } from "@/lib/utils";
 import Navbar from "./Navbar";
 import { Outlet, useLocation } from "@tanstack/react-router";
 
 export default function Layout() {
   const location = useLocation();
 
+  const isPage = location.pathname.startsWith("/page/");
+
   return (
-    <div className="flex h-full w-full flex-col p-1">
-      {!location.pathname.startsWith("/page/") && <Navbar />}
-      <div className="h-full w-full p-3 pt-1">
-        <div className="flex h-full flex-row justify-center gap-8 rounded-xl border-2 border-black bg-gray-200/60 px-3 py-2 dark:border-white dark:bg-gray-900">
+    <div className={cn("flex h-full w-full flex-col", !isPage && "p-1")}>
+      {!isPage && <Navbar />}
+      <div className={cn("h-full w-full", !isPage && "p-3 pt-1")}>
+        <div className="flex h-full flex-row justify-center gap-8 rounded-xl border-2 border-white bg-gray-200/60 px-3 py-2 dark:border-black dark:bg-gray-900">
           <Outlet />
         </div>
       </div>
